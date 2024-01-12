fcfb99028d349b26fdea3a08042c75fac06436be
MadanPatidar <purplecap.help@gmail.com>
2024-01-12 11:40:28 +0530

    1. Implemention the 2 seperate scene for create entity by code & by prefab. 2. Delect curren active all entity by code.

---
+++ ECS_URP/Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Collections;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    public Button NextSceneButton;
+    public Text txtButton;
+
+    public string buttonName;
+    public string sceneName;
+
+    private void Start()
+    {
+        NextSceneButton.onClick.AddListener(NextSceneButtonTap);
+        txtButton.text = buttonName;
+    }
+
+    void NextSceneButtonTap()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        NativeArray<Entity> arrayEntities = entityManager.GetAllEntities();
+        foreach (Entity entity in arrayEntities)
+        {
+            if (entity != null)
+                entityManager.DestroyEntity(entity);
+        }
+
+        Application.LoadLevel(sceneName);    
+    }
+
+}
